fd5466a9628be5e1fbfa1d8cd4a91eb9363fc64d
aincatoni <ain.catoni@gmail.com>
2019-01-14 21:55:19 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
 $(function(){
 
-  var resumen = $('#resumen');
-  var sensacion = $('#sensacion');
-  var probabilidad = $('#probabilidad');
-  var humedad = $('#humedad');
-  var imagen = $('.img-fluid');
-  var escondido = $('#escondido');
+  var dolar = $('#dolar');
+  var euro = $('#euro');
+  var utm = $('#utm');
+  var uf = $('#uf');
 
   
-  var url = 'https://api.darksky.net/forecast/';
-  var key = 'eabc331b530963da3feb3722e2228f4a';
-  var coords = {
-    scl: '-33.4488897,-70.6692655',
-    ccp: '-36.8201352,-73.0443904'
-  }
-
-  var queryParams = ['?exclude=[minutely,hourly,daily,alerts,flags]', 'lang=es', 'units=auto'];
-  var image = {
-  'clear-day':'img/sunnys.svg',
-  'rain':'img/rain.svg',
-  'partly-cloudy-day': 'img/cloudy.svg'
-  }
+  var url = 'https://mindicador.cl/api';
+  var datos = {};
+  var monto = {};
 
 
-
-
-  $('#select').on('change', function (){
+  $(function (){
 
     $.ajax({
-      url: url + key + '/' + coords[$(this).val()] + '?' + queryParams[0] + '&' + queryParams[1] + '&' + queryParams[2],
+      url: url,
       method:'GET',
     }).then(function(data){
-      resumen.text(parseInt(data.currently.temperature) + 'º ' + data.currently.summary);
-      sensacion.text(data.currently.apparentTemperature);
-      probabilidad.text(data.currently.precipProbability * 100 + '%');
-      humedad.text(data.currently.humidity * 100 + '%');
-      imagen.attr('src',image[data.currently.icon]);
-      escondido.removeAttr('hidden', data.curre);
+      datos = data;
+      console.log((data.dolar.valor).toFixed(0),(data.euro.valor).toFixed(0),(data.utm.valor).toFixed(0),(data.uf.valor).toFixed(0));
+      dolar.text((data.dolar.valor).toFixed(0));
+      euro.text((data.euro.valor).toFixed(0));
+      utm.text((data.utm.valor).toFixed(0));
+      uf.text((data.uf.valor).toFixed(0));
     })
+
+  });
+
+  $('#convert').on('click', function(){
+  
+    monto = $('input').val()
+    resultado = monto / datos.dolar.valor;
+    
+    $('#converted-usd').text(resultado.toFixed(2));
+    resultado = monto / datos.euro.valor;
+    $('#converted-eur').text(resultado.toFixed(2));
+    resultado = monto / datos.utm.valor;
+    $('#converted-utm').text(resultado.toFixed(2));
+    resultado = monto / datos.euro.valor;
+    $('#converted-uf').text(resultado.toFixed(2));
 
   });
   
